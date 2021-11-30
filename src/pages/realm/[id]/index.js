4e8993bfdc1f81e380089b6db953e29f414d2611
James Lin <wonyy1988@outlook.com>
2021-11-30 11:07:27 -0800

    feat: add realm page

--- src/pages/realm/[id]/index.js
+++ src/pages/realm/[id]/index.js
@@ -26,11 +26,50 @@ const RealmPage = () => {
       </div>
       <div className={styles.description}>
         Phoebe Heess is a hi-tech fashion lab working with innovative materials and wearables. Think James Bond’s Q, but for all black sportswear. You might have read about them developing the blackest t-shirt in the world. Or when they did a black shirt that doesn’t heat up under the sun. 
-
+        <br /><br />
         Phoebe is a seasoned sportswear designer that has previously collaborated with startups, and also large corps like Adidas. Gabriel is the former Head of Digital at Vice, and has also worked for digital innovation business consultancies.
       </div>
       <div className={styles.subTitle}>
         Key Insights
+      </div>
+
+      <div className={styles.keyInsights}>
+        <div className={styles.item}>
+          <div className={styles.itemName}>
+            $GDN Count:
+          </div>
+          <div className={styles.itemValue}>
+            303.27
+          </div>
+        </div>
+        <div className={styles.item}>
+          <div className={styles.itemName}>
+            FGO Count:
+          </div>
+          <div className={styles.itemValue}>
+            5
+          </div>
+        </div>
+        <div className={styles.item}>
+          <div className={styles.itemName}>
+            Fashion NFT Mints:
+          </div>
+          <div className={styles.itemValue}>
+            4
+          </div>
+        </div>
+        <div className={styles.item}>
+          <div className={styles.itemName}>
+            Meta Source Contributions:
+          </div>
+          <div className={styles.itemValue}>
+            10
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.subTitle}>
+        Patron NFT Tiers
       </div>
     </div>
   )
